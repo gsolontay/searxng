cd961397b4f838f0179db772b25ea093e38f5dc0
gsolontay <79403988+gsolontay@users.noreply.github.com>
2022-07-04 19:21:47 +0400

    Added customization mentions

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name='searxng',
     python_requires=">=3.7",
     version=VERSION_TAG,
-    description="A privacy-respecting, hackable metasearch engine",
+    description="A privacy-respecting, hackable metasearch engine, customized by Georgy Solontay",
     long_description=long_description,
     url=get_setting('brand.docs_url'),
     project_urls={
